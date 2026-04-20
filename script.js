5c7fdc3824935ae55777e4e3623377cb61dd55ac
lcclaysin <lcclaysin@gmail.com>
2026-04-20 20:23:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -32,8 +32,8 @@ let transacoes = [];
 // O detetive blindado: Se a URL NÃO contém "github.io", é o seu VS Code.
 const rodandoNoComputador = !window.location.hostname.includes("github.io");
 
-//const nomeDaColecao = rodandoNoComputador ? "transacoes_teste" : "transacoes";
-const nomeDaColecao = "transacoes_teste";
+const nomeDaColecao = rodandoNoComputador ? "transacoes_teste" : "transacoes";
+//const nomeDaColecao = "transacoes_teste";
 const transacoesRef = collection(db, nomeDaColecao);
 
 if (rodandoNoComputador) {
